12d29a6c332e9e6a819c06624a16ff78eefeba73
Wojciech Balcerzak <wojciech.balcerzak@orpeg.pl>
2019-04-02 13:40:10 +0200

    zad pk5 sp7

---
+++ zadaniepk5.py
@@ -0,0 +1,16 @@
+x = 5
+print(x,end=", ")
+x+=2*x
+print(x)
+#to jest komentarz
+
+x=2+3*2
+print (x)
+# nowa linijka
+
+print ("oliczanie pola")
+a=int(input ("podaj wartosc a =  "))
+b= int(input("podaj wartosc b = "))
+p = a*b/2
+
+print("wynik obliczenia p= ", p)
