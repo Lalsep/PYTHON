9145187cb4c709cd9517190fd745fc485f9cf610
Wojciech Balcerzakf <wbalcerz@gmail.com>
2021-01-17 19:26:21 +0100

    lekcja 4 Turtle

--- LO 1/Turtle/beginning.py
+++ LO 1/Turtle/beginning.py
@@ -2,16 +2,12 @@ import turtle
 
 orpeg = turtle.Turtle()
 
-# orpeg.forward(100)	
-# orpeg.left(45)
-# orpeg.forward(100)
-# orpeg.right(90)
-# orpeg.forward(100)
+
 orpeg.color("blue", "yellow")
 orpeg.begin_fill()
 n = int(input('Podaj liczbę boków n =   '))
 for i in range(n):
-	orpeg.forward(20)
+	orpeg.forward(60)
 	orpeg.left(360/n)
 orpeg.penup()
 orpeg.right(120)
@@ -20,9 +16,9 @@ orpeg.left(120)
 orpeg.left(90)
 orpeg.pendown()
 
-for i in range(4):
+for i in range(3):
 	orpeg.forward(100)
-	orpeg.left(90)
+	orpeg.left(120)
 
 orpeg.end_fill()
 
